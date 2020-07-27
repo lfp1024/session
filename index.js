a72683c59ced89c12384b0654ce27287b5416021
lfp1024 <lfp1024@126.com>
2020-07-27 23:45:31 +0800

    add comment

--- index.js
+++ index.js
@@ -21,10 +21,11 @@ const _CONTEXT_SESSION = Symbol('context#_contextSession');
  * @api public
  */
 
-module.exports = function(opts, app) {
+module.exports = function (opts, app) {
+  // 兼容性处理，参数位置
   // session(app[, opts])
   if (opts && typeof opts.use === 'function') {
-    [ app, opts ] = [ opts, app ];
+    [app, opts] = [opts, app];
   }
   // app required
   if (!app || typeof app.use !== 'function') {
@@ -35,6 +36,7 @@ module.exports = function(opts, app) {
   extendContext(app.context, opts);
 
   return async function session(ctx, next) {
+    debug('return session -----------')
     const sess = ctx[CONTEXT_SESSION];
     if (sess.store) await sess.initFromExternal();
     try {
@@ -62,6 +64,7 @@ function formatOpts(opts) {
   // key
   opts.key = opts.key || 'koa.sess';
 
+  // 兼容性处理，兼容 maxage 防止写错
   // back-compat maxage
   if (!('maxAge' in opts)) opts.maxAge = opts.maxage;
 
@@ -104,6 +107,7 @@ function formatOpts(opts) {
     assert(is.function(ContextStore.prototype.destroy), 'ContextStore.prototype.destroy must be function');
   }
 
+  // 给opts 添加一个生成 id 的方法，调用的时候生成
   if (!opts.genid) {
     if (opts.prefix) opts.genid = () => `${opts.prefix}${uuid()}`;
     else opts.genid = uuid;
@@ -113,6 +117,7 @@ function formatOpts(opts) {
 }
 
 /**
+ * 扩展 app.context 原型，给 ctx 对象添加 session 属性
  * extend context prototype, add session properties
  *
  * @param  {Object} context koa's context prototype
@@ -128,13 +133,19 @@ function extendContext(context, opts) {
   Object.defineProperties(context, {
     [CONTEXT_SESSION]: {
       get() {
+        debug('call ContextSession-------------')
         if (this[_CONTEXT_SESSION]) return this[_CONTEXT_SESSION];
+        // 每次请求都会创建一个 ContextSession 对象
+        debug('new  ContextSession ----------')
         this[_CONTEXT_SESSION] = new ContextSession(this, opts);
         return this[_CONTEXT_SESSION];
       },
     },
     session: {
       get() {
+        // session 是通过 ContextSession 的 get 方法赋值的
+        // 因此需要先添加 [CONTEXT_SESSION] 属性，实例化，然后才能通过其get方法获取session
+        // 每次请求都会创建一个session对象
         return this[CONTEXT_SESSION].get();
       },
       set(val) {
@@ -144,6 +155,8 @@ function extendContext(context, opts) {
     },
     sessionOptions: {
       get() {
+        // 当创建非空session时，会访问 sessionOptions
+        debug('call sessionOptions---------------')
         return this[CONTEXT_SESSION].opts;
       },
     },
